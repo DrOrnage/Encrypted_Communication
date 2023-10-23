a6d09a77de59d0e6e64643e6cdb48033b93939c7
Isaac <isaac_lau@oxcoll.com>
2023-10-23 13:50:09 +0100

    Made a function that asks how many times the lights flashed

---
+++ Communication_test.py
@@ -0,0 +1,50 @@
+from machine import Pin
+import time
+
+red = Pin(15, Pin.OUT)
+green = Pin(14, Pin.OUT)
+redcount = 0
+greencount = 0
+
+def base2_light(s):
+    for i in s:
+        if i == '1':
+            green.toggle()
+            time.sleep(0.5)
+            green.toggle()
+            time.sleep(1)
+            redcount += 1
+        else:
+            red.toggle()
+            time.sleep(0.5)
+            red.toggle()
+            time.sleep(1)
+            greencount += 1
+    redcountans = int(input"How many times did the red light flash?")
+    if redcountans == redcount:
+        print("You're correct!")
+    else:
+        print("You're wrong!")
+    greencountans = int(input"How many times did the green light flash?")
+    if greencountans == greencount:
+        print("You're correct!")
+    else:
+        print("You're wrong!")
+
+def end():
+    green.toggle()
+    red.toggle()
+    time.sleep(0.3)
+    green.toggle()
+    red.toggle()
+    time.sleep(0.3)
+    green.toggle()
+    red.toggle()
+    time.sleep(0.3)
+    green.toggle()
+    red.toggle()
+    time.sleep(1)
+
+base2_light("01010010")
+end()
+base2_light("00110110")
