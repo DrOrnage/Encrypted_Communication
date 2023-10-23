d3b3407fdcd8ef85f2952e77fa0c374e229a0aae
DrOrnage <herrywong8704@gmail.com>
2023-10-23 14:29:27 +0100

    Yellow Light
    Addition of yellow light to indicate the wrong input of users while doing the binary checking.

--- Communication_test.py
+++ Communication_test.py
@@ -3,6 +3,18 @@ import time
 
 red = Pin(15, Pin.OUT)
 green = Pin(14, Pin.OUT)
+yellow = Pin(13, Pin.OUT)
+
+def wrong_input():
+    yellow.toggle()
+    time.sleep(0.3)
+    yellow.toggle()
+    time.sleep(0.3)
+    yellow.toggle()
+    time.sleep(0.3)
+    yellow.toggle()
+    time.sleep(1)
+    return None
 
 def base2_light(s):
     for i in s:
@@ -35,3 +47,4 @@ def end():
 base2_light("01010010")
 end()
 base2_light("00110110")
+wrong_input()
