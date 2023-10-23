d47927c33f685297603788739c60b08b89fee493
Isaac <isaac_lau@oxcoll.com>
2023-10-23 15:21:06 +0100

    Added feature that identifies whether input is right or wrong

--- Communication_test.py
+++ Communication_test.py
@@ -1,10 +1,57 @@
- from machine import Pin
+import math
+from machine import Pin
 import time
+
+user = input("Enter the text: ")
 
 red = Pin(15, Pin.OUT)
 green = Pin(14, Pin.OUT)
-redcount = 0
-greencount = 0
+yellow = Pin(13, Pin.OUT)
+
+def wrong_input():
+    yellow.toggle()
+    time.sleep(0.3)
+    yellow.toggle()
+    time.sleep(0.3)
+    yellow.toggle()
+    time.sleep(0.3)
+    yellow.toggle()
+    time.sleep(1)
+    return None
+
+
+def correct_input():
+    green.toggle()
+    time.sleep(0.3)
+    green.toggle()
+    time.sleep(0.3)
+    green.toggle()
+    time.sleep(0.3)
+    green.toggle()
+    time.sleep(1)
+    return None
+
+
+def string_to_binary(x):
+  #Uses ord function which converts individual string into a value through ASCII table
+  l = [(ord(i)) for i in x]
+  #the bin function converts each value in l to binary. It is slicing from 2 onwards because the function returns a prefix "0b" indicating that it is a binary value
+  m = [bin(i)[2:] for i in l]
+  m = [str(item) for item in m]
+  return m
+
+lst = string_to_binary(user)
+
+
+def string_to_binary(x):
+  #Uses ord function which converts individual string into a value through ASCII table
+  l = [(ord(i)) for i in x]
+  #the bin function converts each value in l to binary. It is slicing from 2 onwards because the function returns a prefix "0b" indicating that it is a binary value
+  m = [bin(i)[2:] for i in l]
+  m = [str(item) for item in m]
+  return m
+
+
 
 def base2_light(s):
     pass
@@ -23,42 +70,14 @@ def base2_light(s):
             greencount += 1
     redcountans = int(input"How many times did the red light flash?")
     if redcountans == redcount:
-        green.toggle()
-        time.sleep(0.5)
-        green.toggle()
-        time.sleep(1)
-        green.toggle()
-        time.sleep(0.5)
-        green.toggle()
-        time.sleep(1)
+        correct_input()
     else:
-        red.toggle()
-        time.sleep(0.5)
-        red.toggle()
-        time.sleep(1)
-        red.toggle()
-        time.sleep(0.5)
-        red.toggle()
-        time.sleep(1)
+        wrong_input()
     greencountans = int(input"How many times did the green light flash?")
     if greencountans == greencount:
-        green.toggle()
-        time.sleep(0.5)
-        green.toggle()
-        time.sleep(1)
-        green.toggle()
-        time.sleep(0.5)
-        green.toggle()
-        time.sleep(1)
+        correct_input()
     else:
-        red.toggle()
-        time.sleep(0.5)
-        red.toggle()
-        time.sleep(1)
-        red.toggle()
-        time.sleep(0.5)
-        red.toggle()
-        time.sleep(1)
+        wrong_input()
     return None
 
 def end():
@@ -75,6 +94,7 @@ def end():
     red.toggle()
     time.sleep(1)
 
-base2_light("01010010")
-end()
-base2_light("00110110")
+for i in lst:
+    base2_light(i)
+    end()
+
